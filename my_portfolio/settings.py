e19c0d774d3d058a242c493926364beb5d7a30fa
michealdayo64 <omotoshomicheal93@gmail.com>
2023-02-08 05:32:49 +0100

    ok

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -101,10 +101,21 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "portfolio_db",
+        'USER': "mikky",
+        'PASSWORD': "password1",
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
