aefa652f05856b784f71ce3ee2bab67ebbf71e26
michealdayo64 <omotoshomicheal93@gmail.com>
2023-02-11 02:29:03 +0100

    ok

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 #DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['54.175.222.217']
+ALLOWED_HOSTS = ['34.233.64.115']
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
